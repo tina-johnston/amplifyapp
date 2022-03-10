df442d8caccdb68c295e37f7d766c7231dacd512
Tina <tina.jston@gmail.com>
2022-03-10 12:59:43 +0000

    auth setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
 
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>changes to app</h1>
+      <header>
+       
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
